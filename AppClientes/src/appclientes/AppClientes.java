181bbeeebd4c8c1e1c71fb54eddb2ad2374a6b93
gabrielmaceira <gabrielmaceira@gmail.com>
2019-09-24 11:46:40 -0300

    primer commit - app creada

---
+++ AppClientes/src/appclientes/AppClientes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appclientes;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class AppClientes {
+
+    public static void main(String[] args) {
+        VentanaCantidad v = new VentanaCantidad();
+        v.setVisible(true);
+    }
+    
+}
